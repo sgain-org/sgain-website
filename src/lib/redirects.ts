2a6df6e6ec190222b072cbd48eaa304e3c45cde5
Tom Heaton <thomas.m.heaton@gmail.com>
2026-06-08 13:02:51 +0100

    1/2 of hero + news migration

--- src/lib/redirects.ts
+++ src/lib/redirects.ts
@@ -9,11 +9,11 @@ export const redirects = [
   },
   {
     from: "assessing-climate-ambition-through-policy-outputs-a-comparative-measure-of-35-major-emitters",
-    to: "/news/assessing-climate-ambition-through-policy-outputs-a-comparative-measure-of-35-major-emitters/",
+    to: "/publications/assessing-climate-ambition-through-policy-outputs-a-comparative-measure-of-35-major-emitters/",
   },
   {
     from: "bath-conference-on-china-and-global-sustainability-transition",
-    to: "/news/bath-conference-on-china-and-global-sustainability-transition/",
+    to: "/publications/bath-conference-on-china-and-global-sustainability-transition/",
   },
   {
     from: "blogs",
@@ -21,7 +21,7 @@ export const redirects = [
   },
   {
     from: "china-and-global-sustainability-transition-outlook-2025",
-    to: "/news/china-and-global-sustainability-transition-outlook-2025/",
+    to: "/publications/china-and-global-sustainability-transition-outlook-2025/",
   },
   {
     from: "china-and-the-global-sustainability-transition-reflecting-on-my-first-conference-experience",
@@ -29,27 +29,27 @@ export const redirects = [
   },
   {
     from: "china-aspires-to-be-an-environmental-leader-how-should-the-rest-of-the-world-engage",
-    to: "/news/china-aspires-to-be-an-environmental-leader-how-should-the-rest-of-the-world-engage/",
+    to: "/publications/china-aspires-to-be-an-environmental-leader-how-should-the-rest-of-the-world-engage/",
   },
   {
     from: "climate-governance-in-china-a-multi-actor-perspective",
-    to: "/news/climate-governance-in-china-a-multiactor-perspective/",
+    to: "/publications/climate-governance-in-china-a-multiactor-perspective/",
   },
   {
     from: "compound-path-dependence-in-green-transitions-a-comparative-analysis-of-ev-policy-coordination-in-indonesia-and-thailand",
-    to: "/news/compound-path-dependence-in-green-transitions-a-comparative-analysis-of-ev-policy-coordination-in-indonesia-and-thailand/",
+    to: "/publications/compound-path-dependence-in-green-transitions-a-comparative-analysis-of-ev-policy-coordination-in-indonesia-and-thailand/",
   },
   {
     from: "dataset-development-in-earth-system-governance-learnings-stakes-and-pathways-for-impact",
-    to: "/news/dataset-development-in-earth-system-governance-learnings-stakes-and-pathways-for-impact/",
+    to: "/publications/dataset-development-in-earth-system-governance-learnings-stakes-and-pathways-for-impact/",
   },
   {
     from: "environmental-governance-in-china",
-    to: "/news/environmental-governance-in-china/",
+    to: "/publications/environmental-governance-in-china/",
   },
   {
     from: "essential-concepts-for-implementing-the-sustainable-development-goals-an-a-z-guide",
-    to: "/news/essential-concepts-for-implementing-the-sustainable-development-goals-an-az-guide/",
+    to: "/publications/essential-concepts-for-implementing-the-sustainable-development-goals-an-az-guide/",
   },
   {
     from: "exploring-europe-reflections-on-the-globalgoals2024-summer-school-and-conference-utrecht-netherlands",
@@ -57,27 +57,27 @@ export const redirects = [
   },
   {
     from: "from-solar-boom-to-green-industrialization-policy-pathways-to-localizing-solar-value-chains-in-pakistan",
-    to: "/news/from-solar-boom-to-green-industrialization-policy-pathways-to-localizing-solar-value-chains-in-pakistan/",
+    to: "/publications/from-solar-boom-to-green-industrialization-policy-pathways-to-localizing-solar-value-chains-in-pakistan/",
   },
   {
     from: "global-climate-governance-remains-resilient-under-trump-20",
-    to: "/news/global-climate-governance-remains-resilient-under-trump-20/",
+    to: "/publications/global-climate-governance-remains-resilient-under-trump-20/",
   },
   {
     from: "global-environmental-politics-amid-geopolitical-turbulence-copy",
-    to: "/news/global-environmental-politics-amid-geopolitical-turbulence/",
+    to: "/publications/global-environmental-politics-amid-geopolitical-turbulence/",
   },
   {
     from: "how-can-the-world-engage-with-china-to-strengthen-global-environmental-governance",
-    to: "/news/how-can-the-world-engage-with-china-to-strengthen-global-environmental-governance/",
+    to: "/publications/how-can-the-world-engage-with-china-to-strengthen-global-environmental-governance/",
   },
   {
     from: "how-much-has-bangladesh-achieved-from-bri",
-    to: "/news/how-much-has-bangladesh-achieved-from-bri/",
+    to: "/publications/how-much-has-bangladesh-achieved-from-bri/",
   },
   {
     from: "is-goal-setting-an-effective-strategy-for-global-sustainability-governance-insights-from-the-sustainable-development-goals-copy",
-    to: "/news/is-goalsetting-an-effective-strategy-for-global-sustainability-governance-insights-from-the-sustainable-development-goals/",
+    to: "/publications/is-goalsetting-an-effective-strategy-for-global-sustainability-governance-insights-from-the-sustainable-development-goals/",
   },
   {
     from: "mercy-mercy-me-things-aint-what-they-used-to-be-field-notes-from-purwakarta-west-java",
@@ -85,7 +85,7 @@ export const redirects = [
   },
   {
     from: "news-and-events",
-    to: "/news/",
+    to: "/publications/",
   },
   {
     from: "reflections-on-the-global-china-workshop-with-redefine-and-sgain",
@@ -101,27 +101,27 @@ export const redirects = [
   },
   {
     from: "steering-political-conflicts-for-climate-stability-the-case-of-china",
-    to: "/news/steering-political-conflicts-for-climate-stability-the-case-of-china/",
+    to: "/publications/steering-political-conflicts-for-climate-stability-the-case-of-china/",
   },
   {
     from: "the-world-needs-climate-change-leadership-its-time-for-china-to-step-up",
-    to: "/news/the-world-needs-climate-change-leadership-its-time-for-china-to-step-up/",
+    to: "/publications/the-world-needs-climate-change-leadership-its-time-for-china-to-step-up/",
   },
   {
     from: "towards-a-just-transition-integrating-fair-share-equity-and-justice-into-climate-transition-plans",
-    to: "/news/towards-a-just-transition-integrating-fair-share-equity-and-justice-into-climate-transition-plans/",
+    to: "/publications/towards-a-just-transition-integrating-fair-share-equity-and-justice-into-climate-transition-plans/",
   },
   {
     from: "unpacking-chinas-climate-policy-mixes-shows-a-disconnect-between-policy-density-and-intensity-in-the-post-paris-era",
-    to: "/news/unpacking-chinas-climate-policy-mixes-shows-a-disconnect-between-policy-density-and-intensity-in-the-postparis-era/",
+    to: "/publications/unpacking-chinas-climate-policy-mixes-shows-a-disconnect-between-policy-density-and-intensity-in-the-postparis-era/",
   },
   {
     from: "varieties-of-local-implementation-for-net-zero-in-china-evidence-from-three-cities-copy",
-    to: "/news/varieties-of-local-implementation-for-net-zero-in-china-evidence-from-three-cities/",
+    to: "/publications/varieties-of-local-implementation-for-net-zero-in-china-evidence-from-three-cities/",
   },
   {
     from: "what-would-it-take-for-china-to-be-a-global-environmental-leader",
-    to: "/news/what-would-it-take-for-china-to-be-a-global-environmental-leader/",
+    to: "/publications/what-would-it-take-for-china-to-be-a-global-environmental-leader/",
   },
   {
     from: "where-is-the-local-community-fieldwork-reflections-on-chinas-involvement-in-indonesias-energy-transition-at-cirata-floating-solar-power-plant",
@@ -129,6 +129,6 @@ export const redirects = [
   },
   {
     from: "will-china-lead-the-way",
-    to: "/news/will-china-lead-the-way/",
+    to: "/publications/will-china-lead-the-way/",
   },
 ] as const;
